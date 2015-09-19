5be9ee35a12ae1f44607b1c5d19a26f6b93f49f2
l-y-s <loganschmidt94@gmail.com>
2015-09-19 02:01:39 -0700

    Refactored GameState and GameStateManager

--- A Future Brightly/src/gameState/GameState.java
+++ A Future Brightly/src/gameState/GameState.java
@@ -6,9 +6,5 @@ public abstract class GameState {
 	protected GameStateManager gsm;
 	
 	public abstract void init();
-	public abstract void update();
-	public abstract void draw(java.awt.Graphics2D g);
-	public abstract void keyPressed(int k);
-	public abstract void keyReleased(int k);
 	
 }
